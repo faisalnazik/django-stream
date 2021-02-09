77f0616e65c150f8f6691b82b7ed55bc102609e5
Faisal Nazik <faisalnazik@gmail.com>
2021-02-09 18:53:26 +0500

    replacing func based views to calls based

--- streams/urls.py
+++ streams/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from . import views
+from streams.views import TvShowsClassifierView
 from django.conf.urls import url, include
 from django.contrib import admin
-
+from django.views.generic import TemplateView
 
 app_name = 'streams'
 
 urlpatterns = [
 
-    path('content', views.tv_show_list, name='tv_show_list'),
+    #path('content/', TemplateView.as_view(template_name="streams/list.html")), 
 
 ]
--- streams/views.py
+++ streams/views.py
@@ -11,26 +11,6 @@ from django.views.generic import RedirectView
 
 
 
-def tv_show_list(request, tag_slug=None):
-    object_list = TvShowsClassifier.published.all()
-    # if tag_slug:
-    #     tag = get_object_or_404(Tag, slug=tag_slug)
-    #     object_list = object_list.filter(tags__in=[tag])
-    paginator = Paginator(object_list, 6) # 6 posts in each page
-    page = request.GET.get('page')
 
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        # If page is not an integer deliver the first page
-        posts = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range deliver last page of results
-        posts = paginator.page(paginator.num_pages)
-
-    return render(request,
-                'streams/list.html',
-                {'page': page,
-                'posts': posts,})
-                #'tag' 	: tag,
-                #'link' : link
+class TvShowsClassifierView():
+    template_name = 'streams/list.html'
